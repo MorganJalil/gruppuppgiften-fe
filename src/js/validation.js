4768a1cbd0fbb4b21d9d0603f02ed67098dcf409
Fanny Schriwer <fanny_schriwer@hotmail.com>
2019-06-14 14:56:30 +0200

    added js file for validation, added input-form to index with html validation. added awesome styling morgan.jalil@medieinstitutet.se

---
+++ src/js/validation.js
@@ -0,0 +1,78 @@
+(function IIFE() {
+    const $form1 = document.getElementById('form');
+
+    const pNumPattern = /\d{6}[+-]\d{4}/;
+    const namePattern = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+    const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  
+    function onSubmitForm(e) {
+      let formValid = true;
+      for (let i = 0; i<e.target.length; i++) {
+        e.target[i].style.borderColor = '';
+        const inputValue = e.target[i].value;
+        const inputDataType = e.target[i].getAttribute('data-type');
+  
+        switch (inputDataType) {
+          case 'personal number':
+            if (!inputValue.match(pNumPattern)) {
+              alert('not personal number');
+              e.target[i].className = 'error';
+              formValid = false;
+            }
+            else {
+              e.target[i].className = '';
+            }
+            break;
+          case 'name':
+            if (!inputValue.match(namePattern)) {
+              console.log('not valid name');
+              e.target[i].className = 'error';
+              formValid = false;
+            }
+            else {
+              e.target[i].className = '';
+            }
+            break;
+
+            case 'email':
+            if (!inputValue.match(emailPattern)) {
+              console.log('not valid email');
+              e.target[i].className = 'error';
+              formValid = false;
+            }
+            else {
+              e.target[i].className = '';
+            }
+            break;
+        }
+      }
+  
+      // If form is not valid, we prevent default action - which is submitting form
+      if (!formValid) {
+        e.preventDefault();
+      }
+    }
+  
+    function onInvalid(e) {
+      e.preventDefault();
+      console.log(e.target.getAttribute('id'));
+      e.target.style.borderColor = 'red';
+    }
+  
+    function registerInvalidListeners(form) {
+      for (let i=0; i<form.length; i++) {
+        form[i].addEventListener('invalid', onInvalid);
+      }
+    }
+  
+    function registerListeners() {
+      $form1.addEventListener('submit', onSubmitForm);
+      registerInvalidListeners($form1);  
+    }
+  
+    function pageLoaded() {
+      registerListeners();
+    }
+  
+    window.pageLoaded = pageLoaded;
+  })();
